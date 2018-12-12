e016d3cd7abf286f7f4ebe31ea94e16b82ef6a8b
折宝鹏 <32212580+BaoPengShe@users.noreply.github.com>
2018-12-12 11:32:50 +0800

    提交了部分单元测试代码

---
+++ ChinaTang/ChinaTangTests/Utils/StringUtilsTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using ChinaTang.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChinaTang.Utils.Tests
+{
+    [TestFixture()]
+    public class StringUtilsTests
+    {
+        [Test()]
+        public void GetMd5ValueTest()
+        {
+            string str = "www.chinatang.net";
+            string md5 = str.GetMd5Value(Encoding.UTF8,Enums.CharCaseEnum.UPPER);
+        }
+    }
+}
