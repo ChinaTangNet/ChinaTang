acfb3c25ceff5c2cc3c0c8478ce075436232fc61
折宝鹏 <32212580+BaoPengShe@users.noreply.github.com>
2018-12-12 11:41:31 +0800

    添加了序列化工具类

---
+++ ChinaTang/ChinaTang/Utils/SerializeUtils.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChinaTang.Utils
+{
+    /// <summary>
+    /// 序列化工具类
+    /// </summary>
+    public class SerializeUtils
+    { 
+    }
+}
