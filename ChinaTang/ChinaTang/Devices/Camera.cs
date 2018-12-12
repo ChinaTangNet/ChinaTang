0179003d88a110cf42752c881b8a368e93ff7bf0
折宝鹏 <32212580+BaoPengShe@users.noreply.github.com>
2018-12-12 11:47:17 +0800

    添加了摄像头

---
+++ ChinaTang/ChinaTang/Devices/Camera.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChinaTang.Devices
+{
+    /// <summary>
+    /// 摄像头
+    /// </summary>
+    public class Camera
+    {
+
+    }
+}
