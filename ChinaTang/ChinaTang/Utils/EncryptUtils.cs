7c678fe8eabe7ab10b3b8b9a937524ddb61f654f
折宝鹏 <32212580+BaoPengShe@users.noreply.github.com>
2018-12-19 09:30:10 +0800

    新增了BASE64加解密的方法

--- ChinaTang/ChinaTang/Utils/EncryptUtils.cs
+++ ChinaTang/ChinaTang/Utils/EncryptUtils.cs
@@ -32,5 +32,33 @@ namespace ChinaTang.Utils
             }
             return str;
         }
+
+        /// <summary>
+        /// 获取字符串的BASE64编码值
+        /// </summary>
+        /// <param name="str">要获取BASE64的字符串</param>
+        /// <param name="encoding">字符编码类型</param>
+        /// <returns>获取到的BASE64字符串</returns>
+        public static string EncryptToBase64Str(string str,Encoding encoding)
+        {
+            if (string.IsNullOrEmpty(str))
+                throw new NonStandardInputException("如果你想获取字符串的BASE64编码值，请确保输入的字符串不为NULL或空！");
+            byte[] bytes = encoding.GetBytes(str);
+            return Convert.ToBase64String(bytes);
+        }
+
+        /// <summary>
+        /// 解码BASE64类型字符串
+        /// </summary>
+        /// <param name="str">要解码的BASE字符串</param>
+        /// <param name="encoding">字符编码格式</param>
+        /// <returns>解码得到的字符串</returns>
+        public static string DecryptFromBase64(string str, Encoding encoding)
+        {
+            if (string.IsNullOrEmpty(str))
+                throw new NonStandardInputException("如果你想解码BASE64类型的字符串，请确保输入的字符串不为NULL或空！");
+            byte[] bytes=Convert.FromBase64String(str);
+            return encoding.GetString(bytes);
+        }
     }
 }
---
+++ ChinaTang/ChinaTangTests/Utils/EncryptUtilsTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using ChinaTang.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChinaTang.Utils.Tests
+{
+    [TestFixture()]
+    public class EncryptUtilsTests
+    {
+        [Test()]
+        public void DecryptFromBase64Test()
+        {
+            Assert.Fail();
+        }
+
+        [Test()]
+        public void EncryptToBase64StrTest()
+        {
+            string base64str = EncryptUtils.EncryptToBase64Str("折宝鹏",Encoding.UTF8);
+            string result = EncryptUtils.DecryptFromBase64(base64str, Encoding.UTF8);
+        }
+    }
+}
