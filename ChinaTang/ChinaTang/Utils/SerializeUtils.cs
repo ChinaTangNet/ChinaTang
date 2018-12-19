103df9ceadd425b8962f1ee883750b1e7ce7257b
折宝鹏 <32212580+BaoPengShe@users.noreply.github.com>
2018-12-19 09:12:55 +0800

    二进制序列化和反序列化

--- ChinaTang/ChinaTang/Utils/SerializeUtils.cs
+++ ChinaTang/ChinaTang/Utils/SerializeUtils.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
@@ -46,12 +47,47 @@ namespace ChinaTang.Utils
         {
             if (string.IsNullOrEmpty(xmlfile))
                 throw new NonStandardInputException("要反序列化的XML文件路径不能为NULL或空！");
-            if (!File.Exists(xmlfile))
-                throw new FileNotFoundException(string.Format("你所要反序列化的文件：{0}不存在！",xmlfile));
             using (XmlReader reader = XmlReader.Create(xmlfile, settings))
             {
                 XmlSerializer xs = new XmlSerializer(typeof(T));
-                object obj= xs.Deserialize(reader);
+                object obj = xs.Deserialize(reader);
+                return obj == null ? default(T) : (T)obj;
+            }
+        }
+
+        /// <summary>
+        /// 将对象序列化为二进制文件
+        /// </summary>
+        /// <param name="obj">要序列化的对象</param>
+        /// <param name="binaryfile">要序列化到的文件</param>
+        /// <param name="iscreateparentpath">在文件的目录结构不存时，是否创建</param>
+        public static void SerializeToBinary(object obj, string binaryfile, bool iscreateparentpath = true)
+        {
+            if(obj == null)
+                throw new NonStandardInputException("如果你想正常进行序列化操作，请确保传入的参数不为空！");
+            //创建目录
+            if (!Directory.Exists(binaryfile) && iscreateparentpath)
+                Directory.CreateDirectory(Path.GetDirectoryName(binaryfile));
+            BinaryFormatter binformat = new BinaryFormatter();
+            using (Stream fsstream = new FileStream(binaryfile,FileMode.Create,FileAccess.Write, FileShare.None))
+            {
+                binformat.Serialize(fsstream, obj);
+            }
+        }
+        /// <summary>
+        /// 将二进制文件反序列化为指定的实例
+        /// </summary>
+        /// <typeparam name="T">反序列化后实例的类型</typeparam>
+        /// <param name="binaryfile">要反序列化的二进制文件路径</param>
+        /// <returns>反序列化得到的实例</returns>
+        public static T DeserializeFromBinary<T>(string binaryfile)
+        {
+            if (string.IsNullOrEmpty(binaryfile))
+                throw new NonStandardInputException("要反序列化的二进制文件路径不能为NULL或空！");
+            BinaryFormatter binformat = new BinaryFormatter();
+            using (Stream fsstream = new FileStream(binaryfile, FileMode.Open, FileAccess.Read, FileShare.None))
+            {
+                object obj=binformat.Deserialize(fsstream);
                 return obj == null ? default(T) : (T)obj;
             }
         }
--- ChinaTang/ChinaTangTests/Utils/SerializeUtilsTests.cs
+++ ChinaTang/ChinaTangTests/Utils/SerializeUtilsTests.cs
@@ -12,6 +12,7 @@ namespace ChinaTang.Utils.Tests
     [TestFixture()]
     public class SerializeUtilsTests
     {
+        [Serializable]
         public class Student
         {
             public string Name { get; set; }
@@ -22,8 +23,22 @@ namespace ChinaTang.Utils.Tests
         [Test()]
         public void SerializeToXmlTest()
         {
-            SerializeUtils.SerializeToXml(new Student { Name = "折宝鹏", Sex = "男", Age = 27 }, @"E:\1\1\1.xml", new XmlWriterSettings { CheckCharacters = false });
-            var reault=SerializeUtils.DeserializeFromXml<Student>( @"E:\1\1\1.xml", new XmlReaderSettings { CheckCharacters = false });
+            SerializeUtils.SerializeToXml(new Student { Name = "折宝鹏", Sex = "男", Age = 27 }, @"E:\1\5\1.xml", new XmlWriterSettings { CheckCharacters = false });
+            var reault = SerializeUtils.DeserializeFromXml<Student>(@"E:\1\4\1.xml", new XmlReaderSettings { CheckCharacters = false });
+        }
+
+        [Test()]
+        public void DeserializeFromBinaryTest()
+        {
+           
+        }
+
+        [Test()]
+        public void SerializeToBinaryTest()
+        {
+            string path = @"E:\1\5\1.dat";
+            SerializeUtils.SerializeToBinary(new Student { Name = "王彦兰", Sex = "男", Age = 27 }, path);
+            var result = SerializeUtils.DeserializeFromBinary<Student>(path);
         }
     }
 }
